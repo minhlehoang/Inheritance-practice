c36d68583b8ebc50c8d624b6d2c21be4cf74bb03
Minh H Le <minhhoangle1104@gmail.com>
2018-03-20 17:04:13 -0600

    5th commit
    Create dropdown boxes for months, years, dates, student status, and faculty positions.

--- TestWithInput.java
+++ TestWithInput.java
@@ -1,5 +1,8 @@
 package PA06;
 
+import java.time.LocalDate;
+
+import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 
 public class TestWithInput {
@@ -13,8 +16,6 @@ public class TestWithInput {
 		String lastName;
 		String phoneNumber;
 		String emailAddress;
-		String student_status; // for student
-		String rankOfFaculty; //for faculty
 		
 		
 //////////////////////////////Declare variables for address input/////////////////////////////////
@@ -25,75 +26,106 @@ public class TestWithInput {
 		int zipCode;
 
 /////////////////////////////Declare variables for date//////////////////////////////////////////////
-		int year; //for faculty
-		int month; //for faculty
-		int day; //for faculty
-
-		// Create an instance of Address using the constructor of Address
+	
 		
-		//
-		
-		// Create an instance of MyDate using the constructor of MyDate
-		// In this example, using the constructor MyDate(int year, int month, int day) 
-		
-		
-			  
-		// create an instance of Faculty using the constructor of Faculty
-		// An instance of Faculty is assigned to superclass Person
-		
-		
-		//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+		String output = "Here is the list" + "\n";
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		int num_per = Integer.parseInt(JOptionPane.showInputDialog("Number of person ?"));
 		personArray = new Person[num_per];
 		for(int i = 0; i < num_per; i++) {
+			//Getting common information of both Students and Faculties
+			/*
 			firstname = JOptionPane.showInputDialog("Firstname ?");
 			lastName = JOptionPane.showInputDialog("Lastname ?");
 			phoneNumber = JOptionPane.showInputDialog("Phone number ?");
-			emailAddress = JOptionPane.showInputDialog("Email Address ?");
-			street = JOptionPane.showInputDialog("Please enter your address");
+			emailAddress = JOptionPane.showInputDialog("Email Address ?");			
 			houseNumber = Integer.parseInt(JOptionPane.showInputDialog("What's your house number?"));
+			street = JOptionPane.showInputDialog("Please enter your street name");
 			city = JOptionPane.showInputDialog("City ?");
 			state = JOptionPane.showInputDialog("State ?");
 			zipCode = Integer.parseInt(JOptionPane.showInputDialog("Zipcode ?"));
+			*/
+			
+			firstname = "Minh";
+			lastName = "Le";
+			phoneNumber = "940404040";
+			emailAddress = "gfif@gmail.com";		
+			houseNumber = 1234;
+			street = "bbb avenue";
+			city = "LA";
+			state = "CA";
+			zipCode = 93030;
+			
 			Address address = new Address(street, houseNumber, city, state, zipCode);
-			student_or_faculty = JOptionPane.showInputDialog("Student or Faculty ?");
-			if(student_or_faculty == "Student") {
-				student_status = JOptionPane.showInputDialog("What year are you? ");
-				Person person = new Student(firstname, lastName, address, phoneNumber, emailAddress, student_status);
+			///////////////////////////////////////////////////////////////////////////////////////////////////////////
+			Object[] options = {"Please select one of these options", "Student", "Faculty"};
+		    JComboBox optionList = new JComboBox(options);
+		    optionList.setSelectedIndex(0);
+		    JOptionPane.showMessageDialog(null, optionList, "Are you a Student or a Faculty ?",
+		        JOptionPane.QUESTION_MESSAGE);
+		    
+		    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		    if(optionList.getSelectedItem() == "Student") {
+				Object[] student_status = {"Please select one of these options", "Freshman", "Sophomore", "Junior", "Senior", "Graduate Student"};
+			    JComboBox student_status_list = new JComboBox(student_status);
+			    student_status_list.setSelectedIndex(0);
+			    JOptionPane.showMessageDialog(null, student_status_list, "What year are you ?",
+			        JOptionPane.QUESTION_MESSAGE);
+				Person person = new Student(firstname, lastName, address, phoneNumber, emailAddress, student_status_list.getSelectedItem().toString());
 				personArray[i] = person;
 			}
 			else {
-				rankOfFaculty = JOptionPane.showInputDialog("Rank of Faculty ?"); 
-				year = Integer.parseInt(JOptionPane.showInputDialog("Year ?"));
-				month = Integer.parseInt(JOptionPane.showInputDialog("Month ?"));
-				day = Integer.parseInt(JOptionPane.showInputDialog("Date ?"));
-				MyDate date = new MyDate(year, month, day);
-				Person person = new Faculty(firstname, lastName, address, phoneNumber, emailAddress, rankOfFaculty, date);
+				Object[] rankOfFaculty = {"Please select one of these options", "Professor", "Teacher Assistant", "Researcher", "Staff"};
+				JComboBox rankOfFaculty_list = new JComboBox(rankOfFaculty);
+				rankOfFaculty_list.setSelectedIndex(0);
+			    JOptionPane.showMessageDialog(null, rankOfFaculty_list, "What's your current position ?",
+			        JOptionPane.QUESTION_MESSAGE);
+			    //Get the year
+			    int year = LocalDate.now().getYear();
+			    Object[] years;
+			    years = new Integer[61];
+				for(int w = 0; w < 61; w++) {
+					years[w] = year - w;
+				}
+				JComboBox years_list = new JComboBox(years);
+				years_list.setSelectedIndex(0);
+			    JOptionPane.showMessageDialog(null, years_list, "Which year were you hired ?",
+			        JOptionPane.QUESTION_MESSAGE);
+			    
+				//Get the month
+				Object[] months;
+				months = new Integer[12];
+				for(int z = 1; z < 13; z++) {
+					months[z-1] = z;
+				}				
+			    JComboBox months_list = new JComboBox(months);
+			    months_list.setSelectedIndex(0);
+			    JOptionPane.showMessageDialog(null, months_list, "Which month were you hired ?",
+			        JOptionPane.QUESTION_MESSAGE);
+			    
+			    //Get the date
+			    Object[] dates;
+			    dates = new Integer[31];
+			    for(int x = 1; x <32; x++) {
+			    	dates[x-1] = x;
+			    }
+			    JComboBox dates_list = new JComboBox(dates);
+			    dates_list.setSelectedIndex(0);
+			    JOptionPane.showMessageDialog(null, dates_list, "Which date were you hired ?",
+			        JOptionPane.QUESTION_MESSAGE);
+				
+				
+				MyDate date = new MyDate(Integer.parseInt(years_list.getSelectedItem().toString()), Integer.parseInt(months_list.getSelectedItem().toString()), Integer.parseInt(dates_list.getSelectedItem().toString()));
+				Person person = new Faculty(firstname, lastName, address, phoneNumber, emailAddress, rankOfFaculty_list.getSelectedItem().toString(), date);
 				personArray[i] = person;
 			}
-			
 		}
+		for(int i=0; i < num_per; i++) {
+			output += personArray[i].toString() + "\n";
+		}
+		JOptionPane.showMessageDialog(null, output);
 		
-		// Hard-coding the array size to 2
-		//personArray = new Person[2];
-		// Storing faculty information in the first element of personArray
-		//personArray [0] = person1;
-		
-		//System.out.println(personArray[0].toString());
-		// This will print out:
-	//	Ramadan , Abdunabi , Address [street=Rockwell Hall East, houseNumber=121, city=Fort Collins, state=Colorado, zipCode=80521] , (970) 491-6227 , ramadan.abdunabi@business.colostate.edu , Professor , MyDate [year=2010, month=11, day=17]
-		
-		// Create an instance of Address using the constructor of Address
-		//Address address2 = new Address("Rockwell Hall West", 231, "Fort Collins", "Colorado", 80521);
-		// create an instance of Student using the constructor of Student
-		// An instance of Student is assigned to superclass Person
-		//Person person2 = new Student("Jim", "Miller", address2, "(970) 431-1212", "jim.miller@gmail.com", "Senior");
-		// Assign instance of Student to the second element of personArray
-		//personArray [1] = person2;
-		
-		//System.out.println(personArray[1].toString());
-		// This will print out:		
-		//Jim , Miller , Address [street=Rockwell Hall West, houseNumber=231, city=Fort Collins, state=Colorado, zipCode=80521] , (970) 431-1212 , jim.miller@gmail.com , Senior
-	}
+		}
 }
---
+++ test.java
@@ -0,0 +1,12 @@
+package PA06;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
